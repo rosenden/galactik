551875e0a9b267da4bd607f95653350ddbf2b8b6
Mathieu Parrino <mathparr@gmail.com>
2025-12-12 11:42:11 +0100

    Fix Portal storybook static assets path

--- apps/portal/.storybook/main.js
+++ apps/portal/.storybook/main.js
@@ -3,5 +3,5 @@ module.exports = {
   stories: ['../src/**/*.stories.@(ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-toolbars'],
   framework: { name: '@storybook/react-vite', options: {} },
-  staticDirs: [{ from: './.storybook/assets', to: '/' }]
+  staticDirs: [{ from: '../.storybook/assets', to: '/' }]
 };
